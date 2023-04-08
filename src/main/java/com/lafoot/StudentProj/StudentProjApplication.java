3ab6c220ddf633fe79e5c44b2a6f436d2d80b0a1
ramanareddy <venkataramanareddygopu@gmail.com>
2023-04-08 12:43:33 +0530

    Basic Setup done

---
+++ src/main/java/com/lafoot/StudentProj/StudentProjApplication.java
@@ -0,0 +1,13 @@
+package com.lafoot.StudentProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lafoot/StudentProj/StudentProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafoot.StudentProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
